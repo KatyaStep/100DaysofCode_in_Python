86c87d43412b7581f1138997fbad0920e975e3e9
Ekaterina Stepanova <katia.yurevna@gmail.com>
2020-07-23 13:50:35 -0700

    Highest Rated Movie Part2

--- Week1/Day4/HighestRatedMovie.py
+++ Week1/Day4/HighestRatedMovie.py
@@ -1,7 +1,17 @@
+"""
+TODO1: Parse the movie_metadata.csv, using csv.DictReader - DONE
+TODO2: Only consider directors with a minimum of 4 movies, otherwise you get misrepresentative data.
+TODO3: Take movies of year >= 1960.
+TODO4: Print the top 20 highest rated directors with their movies ordered desc on rating.
+"""
 import csv
-from collections import defaultdict, namedtuple
+from collections import defaultdict, namedtuple, Counter
 
 movie_csv = 'movies.csv'
+Min_movies = 4  # director with minimum of 4 movies
+Min_year = 1960  # movies of year >= 1960
+top_movies_number = 20  # top of 20 highest rated movies
+
 Movie = namedtuple('Movie', 'title, year, score')
 
 
@@ -22,6 +32,58 @@ def get_movies_by_director(data):
             except ValueError:
                 continue
 
-    print(directors)
+    return directors
 
-get_movies_by_director(movie_csv)
+
+def get_average_score(directors):
+    """Filter directors with < MIN_MOVIES and calculate average score"""
+
+    #cnt = Counter()
+    filtered_director = defaultdict(list)
+
+    for director, movies in directors.items():
+        sum_score = 0
+        avg_score = 0
+        #print(director, len(movies))
+        if len(movies) >= Min_movies:
+            for movie in movies:
+                if movie[1] >= Min_year:
+                    sum_score += movie[2]
+            avg_score = round(sum_score / len(movies), 1)
+            filtered_director[director].append(avg_score)
+    return filtered_director
+
+
+def print_movies(sorted_directors, all_movies):
+    for sort_director, score in sorted_directors.items():
+        print(sort_director, score, end="\n")
+        print("--------------------------")
+        if sort_director in all_movies:
+            for movie in all_movies[sort_director]:
+                print(f'Title: {movie[0]},year: {movie[1]}, imdb: {movie[2]}', end="\n")
+        print()
+
+
+
+
+
+
+
+                # print(movie)
+    #     if movies >= Min_year and len(directors[director]) >= Min_movies:
+    #         #cnt[director] = movies
+    #         filtered_director[director].append(movies[0][2])
+    # print(filtered_director['Gore Verbinski'])
+    # print(directors['Gore Verbinski'])
+
+
+
+   # print(cnt.most_common(20))
+
+
+def main():
+    movies = get_movies_by_director(movie_csv)
+    sorted_directors = get_average_score(movies)
+    print_movies(sorted_directors,movies )
+if __name__ == '__main__':
+    main()
