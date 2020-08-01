e1e9a23d1624fff4e2a6a38efb6576a43e4d998a
Ekaterina Stepanova <katia.yurevna@gmail.com>
2020-07-31 17:49:30 -0700

    RPS game with classes Part2

--- src/Week2/Day13/rps_game.py
+++ src/Week2/Day13/rps_game.py
@@ -1,3 +1,6 @@
+from src.Week2.Day13.rules import Player, AI, Roll
+import random
+
 
 def print_header():
     print('------------------------')
@@ -5,14 +8,57 @@ def print_header():
     print('------------------------')
     print()
 
+
+def get_rolls():
+
+    rolls = [
+        Roll(name='paper', loose='scissors', win='rock'),
+        Roll(name='rock', loose='paper', win='scissors'),
+        Roll(name='scissors', loose='rock', win='paper')
+    ]
+
+    return rolls
+
+
+def get_player_name():
+    # TODO: ask a player his/her name
+    player = input("Player1, your name is: ")
+
+    return player
+
+
+def start_game(player1, player2, rolls):
+    rps = []
+    for roll in rolls:
+        rps.append(roll.get_name())
+
+    print()
+    print(f'RPC Game between {player1.name} and {player2.name}')
+    # print(f'Choose rolls from:{rps}')
+    # print()
+
+    player1_roll = input(f'{player1.name} your roll: ')
+    player2_roll =
+
+    print(player2_roll)
+    #print(f'{player2.name} your roll: {player2_roll} ')
+
+
+def define_winner(roll1, roll2, rolls):
+    pass
+
+
 def main():
     print_header()
     rolls = get_rolls()
-    name = get_players_name()
+    name = get_player_name()
 
     player1 = Player(name)
-    player2 = Player(name)
-    game(player1, player2, roll)
+    player2 = AI('Computer')
+
+    start_game(player1, player2, rolls)
+
+
 
 if __name__ == '__main__':
     main()
--- src/Week2/Day13/rules.py
+++ src/Week2/Day13/rules.py
@@ -1,10 +1,76 @@
+import random
+
+
 class Roll:
-    def __init__(self, name, defeated_by, defeat):
-        self.name = name
-        self.defeated_by = defeated_by
-        self.defeat = defeat
+    rolls = {
+        'paper': {'loose_to': 'scissors', 'win': 'rock'},
+        'rock': {'loose_to': 'paper', 'win': 'scissors'},
+        'scissors': {'loose_to': 'rock', 'win': 'paper'}
+    }
+
+    # def __init__(self, name, loose, win):
+    #     self.__name = name
+    #     self.__loose = loose
+    #     self.__win = win
+    #
+    # def get_name(self):
+    #     return self.__name.upper()
+
+    # def __str__(self):
+    #     return self.str_rep()
+
+    # def str_rep(self):
+    #     return f'Roll: {Roll.rolls}' #, Loose to: {self.__loose}, Win over: {self.__win}'
 
 
 class Player:
     def __init__(self, name):
-        self.name = name
+        self.__name = name
+
+    def get_name(self):
+        return self.__name
+
+# class AI(Player):
+#     pass
+#     # def __init__(self, name):
+#     #     self.name = name
+
+
+class Game:
+    def __init__(self):
+        # self.rolls = {
+        #     'paper': Roll(name='paper', loose='scissors', win='rock'),
+        #     'rock': Roll(name='rock', loose='paper', win='scissors'),
+        #     'scissors': Roll(name='scissors', loose='rock', win='paper')
+        # }
+        self.rolls = Roll.rolls
+
+    def start_game(self):
+        name = input("Player1, your name is: ")
+
+        self.player1 = Player(name)
+        self.player2 = Player('Computer')
+
+        return '{}, {}'.format(self.player1, self.player2)
+
+    def __get_roll(self, player):
+        rps = self.rolls.keys()
+        if player.get_name() != 'Computer':
+            print()
+            print(f'Choose rolls from:{list(rps)}')
+            roll = input(f'{player.get_name()} your roll: ')
+        else:
+            roll = random.choice(list(rps))
+
+        return roll
+
+    def game_round(self):
+        player1_roll = self.__get_roll(self.player1)
+        print(f'{self.player1.get_name()} rolls {player1_roll}')
+        player2_roll = self.__get_roll(self.player2)
+        print(f'{self.player2.get_name()} rolls {player2_roll}')
+
+
+game1 = Game()
+game1.start_game()
+game1.game_round()
